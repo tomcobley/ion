b3fe33ab8f4b6da023671d2571c44019fa9b1da6
andrewlkd <andrewlkd>
2020-06-14 14:16:40 +0100

    Implement 7 day moving average of sleep times

--- battery.c
+++ battery.c
@@ -6,8 +6,8 @@
 #include "curl.h"
 #include "sys.h"
 #include "time.h"
-#include "battery.h"
-#define SEVEN_DAYS_IN_SECONDS (604800)
+#define DAY_IN_SECONDS (86400)
+#define HOUR_IN_SECONDS (3600)
 
 // TODO: improve circular dependency avoidance
 //    #include "battery.h"
@@ -57,48 +57,40 @@ static void log_battery_info(battery_t *battery){
 
   // calculates current time in second past 01/01/1970
   time_t current_time = time(NULL);
-  // creates a struct that stores the current time in the form of
-  // sec/min/hour/day/month/year since 1900
-  struct tm *current = malloc(sizeof(struct tm));
-  current = localtime(&current_time);
 
   fprintf(log_file, "State: %s, Percentage: %d, Time: %s", state_string, battery->percentage, ctime(&current_time));
 
   // TODO: add field in battery struct for 'pre-sleep' (boolean)
 
-  // rawtime is second since 01/01/1970
-  // 7 days = 7 * 24 * 3600 seconds
-  //        = 604800 seconds
+  // rawtime is seconds since 01/01/1970
+  // 1 day = 24 * 3600 seconds
+  //       = 86400 seconds
 
   // stores time of previous line in csv file
   time_t prev_time = current_time;
-  struct tm *prev = malloc(sizeof(struct tm));
-  prev = localtime(&prev_time);
 
   // sum of 7 days previous sleep times stored in minutes past midnight
   int sum_sleep_time = 0;
+  int number_of_sleeps = 0;
   char buff[MAX_LINE_SIZE + 1];
   while (fgets(buff, MAX_LINE_SIZE, analysis_file)){
     time_t temp_time = atol(buff);
-    struct tm *temp = malloc(sizeof(struct tm));
-    temp = localtime(&temp_time);
     // only stores if within 7 days
-    //if(temp_time > (current_time - SEVEN_DAYS_IN_SECONDS)){
-      // only stores if day is different to prev, this means that prev
-      // was the sleep time
-      if(prev->tm_mday != temp->tm_mday){
-	sum_sleep_time += (prev->tm_hour * 60 + prev->tm_min); 
+    if(temp_time > (current_time - 7 * DAY_IN_SECONDS)){
+      // only stores if there is a difference in 4 hours to prev
+      // this means that prev was the sleep time
+      if(prev_time < (temp_time - 4 * HOUR_IN_SECONDS)){
+	struct tm *prev = localtime(&prev_time);
+	// converts prev to minutes past midnight
+	sum_sleep_time += prev->tm_hour * 60 + prev->tm_min; 
+	number_of_sleeps ++;
       }
     prev_time = temp_time;
-      //}
-    free(temp);
+    }
   }
-  printf("Average sleep time = %d\n", sum_sleep_time / 7);
+  printf("Average sleep time = %d\n", sum_sleep_time / number_of_sleeps);
 
-  //fprintf(analysis_file, "%ld,%s,%d\n", current_time, state_string, battery->percentage);
-
-  free(current);
-  free(prev);
+  fprintf(analysis_file, "%ld,%s,%d\n", current_time, state_string, battery->percentage);
   
   if(fclose(log_file) != 0){
     perror("Failed to close battery log file");
@@ -109,7 +101,6 @@ static void log_battery_info(battery_t *battery){
     perror("Failed to close battery analysis file");
     exit(EXIT_FAILURE);
   }
-  
 }
 
 static void monitor_battery(battery_t *battery) {
