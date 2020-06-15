64a90055a9f0fcd3a1259bc75b0b6febe16fac93
andrewlkd <andrewlkd>
2020-06-15 17:50:54 +0100

    Implement testing framework

--- tests/check.c
+++ tests/check.c
@@ -1,1 +1,22 @@
-//test functions here
+#include "check.h"
+
+bool check(bool truth_value, char test_name[], test_state *state) {
+  printf( truth_value ? "/ - PASS" : "X - FAIL" );
+  printf(" -- %s\n", test_name);
+  if (truth_value) {
+    state->successes ++;
+  } else {
+    state->failures ++;
+  }
+  return truth_value;
+}
+
+bool equal_int(int a, int b, char test_name[], test_state *state) {
+  bool success = check( a == b , test_name, state);
+  if (!success) {
+    printf("            EXPECTED a == b\n");
+    printf("                     BUT a = %u  [hex: 0x%x]\n", a, a);
+    printf("                         b = %u  [hex: 0x%x]\n", b, b);
+  }  
+  return success;
+} 
--- tests/check.h
+++ tests/check.h
@@ -1,1 +1,12 @@
-//test headers here
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdint.h>
+
+typedef struct {
+  int successes;
+  int failures;
+} test_state;
+
+bool check(bool truth_value, char test_name[], test_state *state);
+
+bool equal_int(int a, int b, char test_name[], test_state *state);
