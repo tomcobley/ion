c9f71dc82c104c9f0ac8859f5ffccc5c8ff656a2
Tom Cobley <43605582+tomcobley@users.noreply.github.com>
2020-06-16 01:19:52 +0100

    Continue work on init interface, successfully reading input

--- init.c
+++ init.c
@@ -5,6 +5,7 @@
 #include <string.h>
 #include <stdlib.h>
 #include <stdint.h>
+#include <assert.h>
 
 
 // the maximum length a value in the config file can take (some URLs may be long)
@@ -16,18 +17,24 @@
 #define CONFIG_FILE_PATH ("./.config")
 
 
-#define CONFIG_OPTIONS_COUNT (2)
 
-// Note: order of config options must be identical in the two definitions below,
-//    and CONFIG_OPTIONS_COUNT must represent the number of config options in
-//    the two definitions
-// Note: all members of 'struct config' must be pointers
 
+// Note: whenever a config option is added, code in this file must be updated
+//    in multiple places. These places are marked with a comment:
+//       '//CONFIG_UPDATE'
+
+//CONFIG_UPDATE
 typedef struct config {
   char *charge_low_webhook_url;
   char *charge_high_webhook_url;
 } config_t;
 
+//CONFIG_UPDATE
+// Note: the order of the config file must be identical to the order of the
+//    definition below,
+//    and CONFIG_OPTIONS_COUNT must represent the number of config options in
+//    this definition and the definition above.
+#define CONFIG_OPTIONS_COUNT (2)
 char *config_format[CONFIG_OPTIONS_COUNT] = {
   "charge_low_webhook_url",
   "charge_high_webhook_url"
@@ -35,27 +42,61 @@ char *config_format[CONFIG_OPTIONS_COUNT] = {
 
 
 // returns a pointer to a heap allocated struct of type config_t
-//    all fields will be null
 config_t *alloc_config(void) {
-  config_t *config = calloc(1, sizeof(config_t));
+  config_t *config = malloc(sizeof(config_t));
   if (!config) {
     perror("Memory allocation failed for config. "); exit(EXIT_FAILURE);
   }
 
-  uintptr_t config_struct_ptr = (void *) config;
-  for (int i = 0; i < CONFIG_OPTIONS_COUNT; i++) {
-    char *strptr = malloc(MAX_CONFIG_VALUE_LENGTH + 1);
-    if (!strptr) {
-      perror("Memory allocation failed for value. "); exit(EXIT_FAILURE);
-    }
-    *((char **) (config_struct_ptr + i * sizeof(char *))) = strptr;
-  }
   return config;
+}
+
+
+// TODO: move to utils?
+bool streq(const char *a, const char *b) {
+  return strcmp(a, b) == 0;
+}
+
+
+// function to copy the supplied src string into *dest.
+//    if *dest is NULL, allocate memory for it
+static void populate_string(char **dest_ptr, const char *src, int max_length) {
+  if (!*dest_ptr) { *dest_ptr = malloc(max_length); }
+  if (!*dest_ptr) {
+    // if *dest_ptr is still NULL
+    perror("Memory allocation failed. "); exit(EXIT_FAILURE);
+  }
+  strncpy(*dest_ptr, src, max_length);
+}
+
+// set the value of the config option corresponding to the supplied key
+//    if the struct member is not initialised, allocate memory for it
+static void set_config_option(config_t *config, char *key, char *value) {
+  //CONFIG_UPDATE
+  if (streq(key, "charge_low_webhook_url")) {
+    populate_string(&config->charge_low_webhook_url, value, MAX_CONFIG_VALUE_LENGTH);
+  } else if (streq(key, "charge_high_webhook_url")) {
+    populate_string(&config->charge_high_webhook_url, value, MAX_CONFIG_VALUE_LENGTH);
+  } else {
+    // TODO:
+    assert(false);
+  }
 }
 
 // frees a config struct created by alloc_config
 void free_config(config_t *config) {
-  // TODO: implement
+  //CONFIG_UPDATE
+  free(config->charge_low_webhook_url);
+  free(config->charge_high_webhook_url);
+  free(config);
+}
+
+
+// print config (for debugging purposes)
+void print_config(config_t *config) {
+  //CONFIG_UPDATE
+  printf("charge_low_webhook_url: %s\n", config->charge_low_webhook_url);
+  printf("charge_high_webhook_url: %s\n", config->charge_high_webhook_url);
 }
 
 
@@ -65,58 +106,36 @@ void free_config(config_t *config) {
 // if successful, the config values will be saved to the supplied struct
 bool read_config(FILE *config_file, config_t *config) {
 
-  // printf("config %x\n", (config) );
-  // config->charge_low_webhook_url = "low-----------";
-  // config->charge_high_webhook_url = "highkajshdflkashdflksadfhd";
-
-
-  //
-  // printf("addressA: %x\n", &(config->charge_low_webhook_url));
-  // printf("addressB: %x\n", &(config->charge_high_webhook_url));
-  // printf("valA: %x\n", config->charge_low_webhook_url);
-  // printf("valB: %x\n", config->charge_high_webhook_url);
-  //
-  // printf("strA: %s\n", config->charge_low_webhook_url);
-  // printf("strB: %s\n", config->charge_high_webhook_url);
-  //
-  // uintptr_t ptrA = config;
-  // uintptr_t ptrB = ptrA + 8;
-  //
-  // printf("ptrA: %x\n", ptrA);
-  // printf("ptrB: %x\n", ptrB);
-  //
-  // printf("strA: %s\n", *((char *) ptrA));
-  // printf("strB: %s\n", *((char *) ptrB));
-
-
-
-
   int count = 0;
   char line_buffer[MAX_CONFIG_LINE_LENGTH + 1];
-  uintptr_t config_struct_ptr = (void *) config;
 
   while (fgets(line_buffer, MAX_CONFIG_LINE_LENGTH, config_file)) {
 
     // tokenize at '=', line_buffer will point to key of line
     strtok(line_buffer, "=");
-    if (strcmp(line_buffer, config_format[count]) != 0) {
+    if (!streq(line_buffer, config_format[count])) {
       // key string is not correct according to config_format, so return false
-      printf("incorrect string, expected %s, got %s\n", config_format[count], line_buffer);
+      printf("Incorrect string, expected %s, got %s\n", config_format[count], line_buffer);
       return false;
     }
 
+    printf("line_buffer: %s\n", line_buffer);
+
+
     // key string is correct, so extract value
     //    (use NULL parameter to strtok to continue tokenizing line_buffer)
-    strtok(NULL, ";");
+    char *value = strtok(NULL, ";");
 
-    // line_buffer now points to value for this line; save this value to config
-    strncpy((void *) (config_struct_ptr + (count * sizeof(char *))),
-            line_buffer, MAX_CONFIG_VALUE_LENGTH);
+    printf("val: %s\n", value);
+
+
+    // line_buffer still points to key, val points to value for this line;
+    //    save this value to relevant member in config
+    set_config_option(config, line_buffer, value);
 
     count++;
   }
 
-  printf("line_buffer: %s\n", line_buffer);
 
   if (count < CONFIG_OPTIONS_COUNT) {
     // not enough options specified, return false
@@ -128,35 +147,90 @@ bool read_config(FILE *config_file, config_t *config) {
   return true;
 }
 
+
+static void update_config_option(char **dest, const char *prompt) {
+  printf("%s:\n", prompt);
+  if (*dest) {
+    // *dest is not NULL, so present default option
+    printf("- or press ENTER to use \"%s\"\n", *dest);
+  }
+
+  char response[MAX_CONFIG_VALUE_LENGTH];
+  fgets(response, MAX_CONFIG_VALUE_LENGTH, stdin);
+  // strip \n from response
+  if (response[strlen(response - 1)] == '\n') {
+    response[strlen(response - 1)] = '\0';
+  }
+
+  if (streq(response, "") && !*dest) {
+    // empty response not allowed as no default exists, so ask again
+    printf("%s\n", "Please enter a response: \n");
+    update_config_option(dest, prompt);
+  } else if (streq(response, "")) {
+    // use default (i.e. don't change value of dest)
+    printf("Using default.\n");
+  } else {
+    populate_string(dest, response, MAX_CONFIG_VALUE_LENGTH);
+  }
+}
+
+// take user input to populate the supplied config struct.
+//    if values already exist in config, the user will be able to leave these
+//    by entering an empty string for a new value
+void get_config(config_t *config) {
+
+  for(int i = 0; i < CONFIG_OPTIONS_COUNT; i++) {
+    char *key = config_format[i];
+    if (streq(key, "charge_low_webhook_url")) {
+      update_config_option(&config->charge_low_webhook_url,
+        "Enter URL of webhook to be activated when battery charge reaches LOWER threshold");
+    } else if (streq(key, "charge_high_webhook_url")) {
+      update_config_option(&config->charge_high_webhook_url,
+        "Enter URL of webhook to be activated when battery charge reaches UPPER threshold");
+    }
+
+  }
+}
+
+
+
 void init(config_t *config) {
 
-  FILE *config_file = fopen(CONFIG_FILE_PATH, "r");
+  FILE *config_file = fopen(CONFIG_FILE_PATH, "a+");
   if (config_file == NULL) {
     perror("Failed to open/create config file. "); exit(EXIT_FAILURE);
   }
+
+  // rewind to start of file
+  rewind(config_file);
 
   bool config_valid = read_config(config_file, config);
 
   if (config_valid) {
     printf("valid config\n");
-    printf("%s\n", config->charge_low_webhook_url);
-    printf("%s\n", config->charge_high_webhook_url);
+    print_config(config);
   } else {
     printf("invalid config\n");
   }
 
+  get_config(config);
+
+  printf("Configuration updated successfully:\n");
+  print_config(config);
+
 }
 
 
-int main(int argc, char const *argv[]) {
 
-  printf("%d\n", MAX_CONFIG_LINE_LENGTH);
+
+int main(int argc, char const *argv[]) {
 
   config_t *config = alloc_config();
 
   init(config);
 
   free_config(config);
+
 
 
 }
